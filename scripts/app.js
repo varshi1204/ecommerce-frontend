e544e1c629449ca3788f5750beb65c845a8076c0
Varshitha <koramutlavarshitha1208@gmail.com>
2026-01-11 16:41:26 +0530

    Add responsive mobile navigation with hamburger menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,26 @@
 console.log("E-commerce Website Loaded");
+
+/*
+//Seclect elements
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+//Toggle mobile view
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("E-commerce Website Loaded");
+
+    // Select elements
+    const hamburger = document.getElementById("hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+
+    // Toggle mobile menu
+    hamburger.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+});
